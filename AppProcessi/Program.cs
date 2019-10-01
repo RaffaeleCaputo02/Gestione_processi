3f9d1ec3ef1d6a00ce6744521e527b82b13f485c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-01 09:56:42 +0200

    programma finito

--- AppProcessi/Program.cs
+++ AppProcessi/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace AppProcessi
 {
@@ -10,7 +12,28 @@ namespace AppProcessi
     {
         static void Main(string[] args)
         {
-            int n = 0;
+            Console.WriteLine("Esercitazione sulla gestione dei processi");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe", @"C:\Users\docente\Source\Repos\Gestione_processi\AppProcessi\HelloWorld.txt");
+            Process.Start("Chrome.exe", @"https://www.youtube.it");
+            var app = new Process();
+            app.StartInfo.FileName = @"Notepad.exe";
+            app.StartInfo.Arguments = @"C:\Users\docente\Source\Repos\Gestione_processi\AppProcessi\HelloWorld.txt";
+            app.Start();
+            app.PriorityClass = ProcessPriorityClass.RealTime;
+            //app.WaitForExit();
+            
+
+            var processes = Process.GetProcesses();
+            foreach(var p in processes)
+            {
+                if(p.ProcessName=="notepad")
+                {
+                    p.Kill();
+                }
+            }
+            Console.WriteLine("Programma terminato!");
+            Console.ReadLine();
 
         }
     }
